65666fb2037f08ae1b2183e9cf4c68674a56b9d2
Rodrigo Muñoz Muñoz <rodrigo.mcuadrada@gmail.com>
2017-08-09 11:45:25 -0500

    module file alpha Version

--- IEM.py
+++ IEM.py
@@ -1,1 +1,21 @@
 """This Module contains basic functions that I use in various programs"""
+
+# Variable Definitions
+
+__version__ = "Alpha"
+__author__ = "Rodrigo 'ItsPaper' Muñoz"
+__email__ = "rodrigo.mcuadrada@gmail.com"
+__license__ = "MIT"
+__date__ = "9th of August 2017"
+
+# functions
+
+
+def cls():
+    """This function clears the shell screen"""
+    print("\n" * 50)
+
+
+def pause():
+    """This function pauses the program until the user presses enter key"""
+    x = input("PRESS ENTER TO CONTINUE...")
